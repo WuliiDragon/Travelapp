34dcdad8fd39b4707311f4c60f68dba94ca61c0a
u1095279147129 <1095279147129@tianchi.com>
2017-11-23 16:48:49 +0800

    商家详情完善了门票 zyl 2017年11月23日16:48:39

--- app/src/main/java/wlxy/com/travelapp/fragment/TicketFragment.java
+++ app/src/main/java/wlxy/com/travelapp/fragment/TicketFragment.java
@@ -14,9 +14,9 @@ import wlxy.com.travelapp.R;
 import wlxy.com.travelapp.adapter.MerChantAdapter;
 import wlxy.com.travelapp.adapter.TicketAdapter;
 import wlxy.com.travelapp.model.MerChantModel;
+import wlxy.com.travelapp.model.TicketModel;
 
 /**
- *
  * @author dragon
  * @date 2017/11/22
  */
@@ -25,31 +25,43 @@ public class TicketFragment extends Fragment {
     private ListView ticketListView;
     private ListView merChantListView;
     private final int RIGHTSTATUS = 200;
-    private ArrayList<MerChantModel> merChantModelList;
+    private ArrayList<TicketModel> ticketModelList;
     private TicketAdapter ticketAdapter;
 
-//    public static TicketFragment newInstance(String name, string passwd) {
-//        TicketFragment newFragment = new TicketFragment();
-//        Bundle bundle = new Bundle();
-//        bundle.putString("name", name);
-//        bundle.putString("passwd", passwd);
-//        newFragment.setArguments(bundle);
-//
-//        return newFragment;
-//
-//    }
+    public void setTicketAdapter(TicketAdapter ticketAdapter) {
+        this.ticketAdapter = ticketAdapter;
+    }
+
+    public TicketAdapter getTicketAdapter() {
+        return ticketAdapter;
+    }
+
+    /**
+     * 静态工厂方法需要一个int型的值来初始化fragment的参数，
+     * 然后返回新的fragment到调用者
+     */
+
+    public static TicketFragment newInstance(ArrayList<TicketModel> ticketModelList) {
+        TicketFragment f = new TicketFragment();
+        Bundle args = new Bundle();
+        args.putSerializable("ticketModelList", ticketModelList);
+        f.setArguments(args);
+        return f;
+    }
 
 
-//    @Nullable
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        View view = inflater.inflate(R.layout.home_layout, container, false);
-//        ticketListView = (ListView) view.findViewById(R.id.merchant_listView);
-//        ticketAdapter = new TicketAdapter();
-//
-//
-//        return;
-//
-//
-//    }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Bundle args = new Bundle();
+        ticketModelList = (ArrayList<TicketModel>) getArguments().getSerializable("ticketModelList");
+
+        View view = inflater.inflate(R.layout.home_layout, container, false);
+        ticketListView = (ListView) view.findViewById(R.id.merchant_listView);
+
+        ticketAdapter = new TicketAdapter(getActivity(), R.layout.item_ticket, ticketModelList);
+        ticketListView.setAdapter(ticketAdapter);
+        return view;
+
+    }
 }
--- app/src/main/java/wlxy/com/travelapp/main/MerChantDetailActivity.java
+++ app/src/main/java/wlxy/com/travelapp/main/MerChantDetailActivity.java
@@ -9,12 +9,16 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 
+import com.alibaba.fastjson.JSON;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -22,8 +26,12 @@ import java.util.List;
 
 import wlxy.com.travelapp.R;
 import wlxy.com.travelapp.adapter.MerChantDetailAdapter;
+import wlxy.com.travelapp.adapter.TicketAdapter;
 import wlxy.com.travelapp.fragment.MineFragment;
 import wlxy.com.travelapp.fragment.TicketFragment;
+import wlxy.com.travelapp.model.MerChantModel;
+import wlxy.com.travelapp.model.TicketModel;
+import wlxy.com.travelapp.utils.AppController;
 import wlxy.com.travelapp.utils.HttpUtils;
 import wlxy.com.travelapp.utils.utils;
 
@@ -36,6 +44,7 @@ public class MerChantDetailActivity extends AppCompatActivity {
     private TabLayout tabBarTitle;
     private ViewPager tabBarViewPager;
     private String bid;
+    private ArrayList<TicketModel> ticketModelList;
 
     private ArrayList<String> titleList = new ArrayList<String>() {{
         add("门票");
@@ -43,13 +52,7 @@ public class MerChantDetailActivity extends AppCompatActivity {
         add("纪念品");
     }};
 
-    private ArrayList<Fragment> fragmentList = new ArrayList<Fragment>() {{
-        add(new TicketFragment());
-        add(new MineFragment());
-        add(new MineFragment());
-    }};
-
-
+    private ArrayList<Fragment> fragmentList;
     private MerChantDetailAdapter merChantDetailAdapter;
 
     @Override
@@ -62,6 +65,14 @@ public class MerChantDetailActivity extends AppCompatActivity {
         setContentView(R.layout.merchant_detail_layout);
         tabBarTitle = (TabLayout) findViewById(R.id.tab_title);
         tabBarViewPager = (ViewPager) findViewById(R.id.tab_view_pager);
+        ticketModelList = new ArrayList<TicketModel>();
+
+        fragmentList = new ArrayList<Fragment>() {{
+            add(TicketFragment.newInstance(ticketModelList));
+            add(new MineFragment());
+            add(new MineFragment());
+        }};
+
 
         //viewpager的适配
         merChantDetailAdapter = new MerChantDetailAdapter(getSupportFragmentManager(), titleList, fragmentList);
@@ -70,19 +81,40 @@ public class MerChantDetailActivity extends AppCompatActivity {
         tabBarTitle.setTabMode(TabLayout.MODE_FIXED);
 
 
-        HttpUtils request = new HttpUtils(utils.BASE + "/business/findById.action?bid=" + bid, null, new Response.Listener<JSONObject>() {
+        HttpUtils request = new HttpUtils(utils.BASE + "/business/findById.action?bid=" + "2511150102", null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                Log.v("data", response.toString());
+                try {
+                    String status = response.getString("status");
+                    if (status.equals("200")) {
+                        JSONObject data = response.getJSONObject("data");
+                        JSONArray ticket = data.getJSONArray("ticket");
+
+                        for (int i = 0; i < ticket.length(); i++) {
+                            JSONObject item = (JSONObject) ticket.get(i);
+                            TicketModel ticketModel = JSON.parseObject(item.toString(), TicketModel.class);
+                            ticketModelList.add(ticketModel);
+                        }
+
+                        TicketFragment tf = (TicketFragment) fragmentList.get(0);
+                        tf.getTicketAdapter().notifyDataSetChanged();
+                    } else {
+                        String msg = response.getString("msg");
+                        Toast.makeText(MerChantDetailActivity.this, msg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                Toast.makeText(MerChantDetailActivity.this, error.toString(), Toast.LENGTH_LONG).show();
             }
         });
 
+        AppController.getInstance().addToRequestQueue(request);
 
     }
 
